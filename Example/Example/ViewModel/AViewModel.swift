ccc3be300cd3e1c9454c5c0744428032d44fe075
ApterKing <wangcccong@outlook.com>
2017-05-24 16:24:06 +0800

    添加Pod

---
+++ Example/Example/ViewModel/AViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AViewModel.swift
+//  Example
+//
+//  Created by wangcong on 24/05/2017.
+//  Copyright © 2017 ApterKing. All rights reserved.
+//
+
+import UIKit
+
+class AViewModel: NSObject {
+
+}
---
+++ Pod/Classes/Model/AModel.swift
@@ -0,0 +1,13 @@
+//
+//  AModel.swift
+//  Example
+//
+//  Created by wangcong on 24/05/2017.
+//  Copyright © 2017 ApterKing. All rights reserved.
+//
+
+import UIKit
+
+class AModel: NSObject {
+
+}
